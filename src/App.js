b8580c86bc62cc295e89766e92e7acc17d445814
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2017-12-11 11:39:49 +0100

    add Logic in App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,57 @@ import './App.css';
 import Spinner from './components/Spinner/Spinner';
 import Header from './components/Header/Header';
 
+import { getUsers } from './helpers/api';
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: "",
+      users: [],
+      isLoading: false,
+    }
+  }
+
+  handleSearchInput = (event) => {
+    console.log('handleSearchInput')
+    this.setState({
+      search: event.target.value,
+    });
+  }
+
+  handleClick = () => {
+    console.log('handleClick')
+    this.setState({
+      isLoading: true,
+    });
+    getUsers(this.state.search)
+      .then((result) => {
+        this.setState({
+          users: result.data.items,
+          isLoading: false,
+        });
+      });
+  }
+
   render() {
+    const { isLoading, search } = this.state;
+    const loading = isLoading
+      ? <Spinner message='Loading' />
+      : <p>{search}</p>;
+    
     return (
       <div>
         <Header title="github search" />
-        <Spinner message='Loading' />        
+        <div className="container">
+          <div className="row">
+            <div className="column column-50 column-offset-25">
+              <input onChange={this.handleSearchInput} type="text"/>
+              <button onClick={this.handleClick} className="search-button">search</button>
+              { loading }
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
