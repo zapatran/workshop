d5760fbe231b66e6207b5d37cbfebbc2945b6c21
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2017-12-11 11:09:17 +0100

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Spinner from './components/Spinner/Spinner';
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Header title="github search" />
         <Spinner message='Loading' />        
       </div>
     );
